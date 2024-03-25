614b55cdcec678c553bb82fd1b2833872c2b0666
01fe22bca153 <158065731+01fe22bca153@users.noreply.github.com>
2024-03-25 19:20:11 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+    private String name;
+    private int employeeId;
+    protected double salary;
+
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public void displayEmployeeDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Salary: $" + salary);
+    }
+}
